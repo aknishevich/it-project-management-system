835d93b1716586967105a748cf879819bf8637ae
Alexander Knyshevych <aknishevich@gmail.com>
2019-12-27 23:18:54 +0200

    small changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
@@ -15,11 +16,12 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, \Swift_Mailer $mailer): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
+
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
@@ -34,9 +36,15 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            $message = (new \Swift_Message('Log in email'))
+                ->setFrom('pm.aknishevich@gmail.com')
+                ->setTo($user->getEmail())
+                ->setBody("<strong>{$user->getName()}</strong>" . ' you\'ve been successfully registered on project-manager website. To log into your account follow the link ' . $this->generateUrl('login', [], UrlGeneratorInterface::ABSOLUTE_URL), 'text/html');
+            $mailer->send($message);
+
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('greeting');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -85,6 +85,8 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator
             return new RedirectResponse($targetPath);
         }
 
+
+
         return new RedirectResponse($this->urlGenerator->generate('greeting'));
     }
 
